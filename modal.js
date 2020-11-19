33e390b9bbe28477bc7d13b75ec20588ec74b2e9
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2020-11-19 13:09:31 +0100

    Nemtudom

--- modal.js
+++ modal.js
@@ -1,17 +1,18 @@
-var modal = document.getElementsByClassName("mymodal");
-var btn = document.getElementsByClassName("modal-open");
-var cls = document.getElementsByClassName("close")[0];
+'use strict'
+const modal = document.querySelectorAll(".mymodal");
+const btn = document.querySelectorAll(".modal-open");
+const cls = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.addEventListener('click', function () {
   modal.style.display = "block";
-}
+})();
 
-cls.onclick = function() {
+cls.addEventListener('click', function () {
   modal.style.display = "none";
-}
+})();
 
-window.onclick = function(event) {
+window.onclick = (function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+})();
