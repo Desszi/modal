1ea41983c54aa36b20bf47fda333b9d47923092c
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2020-11-19 12:33:08 +0100

    Félkész

---
+++ modal.js
@@ -0,0 +1,17 @@
+var modal = document.getElementsByClassName("mymodal");
+var btn = document.getElementsByClassName("modal-open");
+var cls = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+cls.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
